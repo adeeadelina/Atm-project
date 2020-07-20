9a2fcc6b64706a87564675fcf4b751eefca2d581
adeeadelina <moisiadelina@gmail.com>
2020-07-20 15:30:44 +0300

    Initial commit

---
+++ src/main/java/com/example/Atmproject/AtmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Atmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmProjectApplication.class, args);
+	}
+
+}
