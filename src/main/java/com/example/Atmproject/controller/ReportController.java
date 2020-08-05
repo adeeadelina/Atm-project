94352785b3b99437a58cd0858e0deb101e5b710b
adeeadelina <moisiadelina@gmail.com>
2020-08-05 16:29:16 +0300

    New feature: customizable report.

--- src/main/java/com/example/Atmproject/controller/ReportController.java
+++ src/main/java/com/example/Atmproject/controller/ReportController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,12 @@ public class ReportController {
     @GetMapping("/api/report")
     public ResponseEntity<String> getReport() throws Exception {
         PDFGenerator pdfGenerator = new PDFGenerator(transactionHistoryService.createTransactionsTable());
+        return new ResponseEntity<>("Report successfully created!", HttpStatus.OK);
+    }
+
+    @GetMapping("/api/report/{mins}")
+    public ResponseEntity<String> getReportByTime(@PathVariable("mins") int mins) throws Exception {
+        PDFGenerator pdfGenerator = new PDFGenerator((transactionHistoryService.createFilteredTable(mins)));
         return new ResponseEntity<>("Report successfully created!", HttpStatus.OK);
     }
 }
--- src/main/java/com/example/Atmproject/service/TransactionHistoryService.java
+++ src/main/java/com/example/Atmproject/service/TransactionHistoryService.java
@@ -5,6 +5,7 @@ import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Table;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.TreeSet;
 
 @Service
@@ -12,7 +13,7 @@ public class TransactionHistoryService {
     private TreeSet<TransactionEntity> transactions;
 
     public TransactionHistoryService() {
-        transactions = new TreeSet<>((o1,o2) -> o2.compareTo(o1));
+        transactions = new TreeSet<>((o1, o2) -> o2.compareTo(o1));
     }
 
     public TreeSet<TransactionEntity> getTransactions() {
@@ -30,6 +31,27 @@ public class TransactionHistoryService {
         table.addCell(new Cell().add(entity.getTimeFormatPDF()));
         table.addCell(new Cell().add(entity.getRequestFormatPDF()));
         table.addCell(new Cell().add(entity.getResponseFormatPDF()));
+
+        return table;
+    }
+
+    public Table createFilteredTable(int mins) {
+
+        float[] pointColumnWidths = {140F, 200F, 200F};
+        Table table = new com.itextpdf.layout.element.Table(pointColumnWidths);
+
+        for (TransactionEntity entity : transactions) {
+            if(entity.getTime().isAfter(LocalDateTime.now().minusMinutes(mins))) {
+                table.addCell(new Cell().add(entity.getTimeFormatPDF()));
+                if (entity.getType().equals("transaction")) {
+                    table.addCell(new Cell().add(entity.getTransactionRequestFormatPDF()));
+                    table.addCell(new Cell().add(entity.getTransactionResponseFormatPDF()));
+                } else {
+                    table.addCell(new Cell().add(entity.getRequestFormatPDF()));
+                    table.addCell(new Cell().add(entity.getResponseFormatPDF()));
+                }
+            }
+        }
 
         return table;
     }
