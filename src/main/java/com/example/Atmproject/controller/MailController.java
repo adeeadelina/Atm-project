6b139c0134342ca77623606a30dd7151631469a2
adeeadelina <moisiadelina@gmail.com>
2020-08-10 10:03:09 +0300

    Added mail controller to print emails to pdf file.

--- src/main/java/com/example/Atmproject/controller/MailController.java
+++ src/main/java/com/example/Atmproject/controller/MailController.java
@@ -1,17 +1,21 @@
 package com.example.Atmproject.controller;
 
-import com.example.Atmproject.service.CashWithdrawalService;
+import com.example.Atmproject.pdf.PDFGenerator;
+import com.example.Atmproject.service.ATMMailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MailController {
     @Autowired
-    private CashWithdrawalService cashWithdrawalService;
+    private ATMMailService atmMailService;
 
     @GetMapping("/api/atm-inbox")
-    public String showMails() {
-        return cashWithdrawalService.getMailList().toString();
+    public ResponseEntity<String > showMails() throws Exception {
+        PDFGenerator pdfGenerator = new PDFGenerator("mail", atmMailService.createTable());
+        return new ResponseEntity<>("Check sample.pdf for your inbox!", HttpStatus.OK);
     }
 }
--- src/main/java/com/example/Atmproject/util/MailNotification.java
+++ src/main/java/com/example/Atmproject/util/MailNotification.java
@@ -1,10 +1,19 @@
 package com.example.Atmproject.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class MailNotification {
     private final String messageToSend;
+    private final LocalDateTime time;
 
     public MailNotification(String message) {
+        this.time = LocalDateTime.now();
         this.messageToSend = "ATM machine" + "\n" + "To: fillMeUpPlease@superbancomat.com" + "\n" + message;
+    }
+
+    public String getTime() {
+        return time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 
     public String getMessageToSend() {
@@ -14,4 +23,5 @@ public class MailNotification {
     public String toString() {
         return messageToSend;
     }
+
 }
